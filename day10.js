4da623b0c9f20816a00357568eddde5fa3de6405
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-02-04 16:39:55 +0530

    Create day10.js

---
+++ day10.js
@@ -0,0 +1,55 @@
+var a=2;
+var b= 3;
+
+// Assignment Operators
+// a+=2;// Addition assignment
+// console.log(a);
+// a-=2;// subtraction assignment
+// console.log(a);
+// a*=2; // multiplication assignment
+// console.log(a);
+// a**=2; // Exponential assignment
+// console.log(a);
+// a/=2; // division assignment
+// console.log(a);
+// a%=2;// remainder assignment
+// console.log(a);
+
+//Comparison Operators
+console.log(2 =='2');
+console.log(2 ==='2');
+console.log(2 !='2');
+console.log(2 !=='2');
+console.log(2 >3);
+console.log(2 >=3);
+console.log(2<3);
+console.log(2<=3);
+
+//Arithmatical operators
+// console.log(a % b);
+// var c= ++a; //perfix
+// console.log(c);
+// c = a++;  //sufix
+// console.log(c);
+// c=-b;
+// console.log(c);
+// c=+'5';
+// console.log('c:', c , "Type of :", typeof(c));
+// console.log(a**b);
+
+//logical operators
+// a= true;
+// b=false;
+// console.log(a && b);
+// console.log(a || b);
+// console.log(!(a && b));
+
+// Concatination Operator
+// let d ="Hello";
+// d+=" world";
+// console.log(d);
+// console.log(d + "!");
+
+// conditional operator
+// a = 1>2 ? "yes": "no" ;
+// console.log(a);
