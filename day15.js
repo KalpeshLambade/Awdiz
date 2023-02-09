3a5c0c5dd8ba730927ab5404ef7ccfc495668e57
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-02-09 20:13:35 +0530

    up

---
+++ day15.js
@@ -0,0 +1,11 @@
+// logical operators
+// a= true;
+// b=false;
+// console.log(a && b);
+// console.log(a || b);
+// console.log(!(a && b));
+// console.log((a && b) && (a || b));
+// console.log(((a && b) && (a || b)) || (!(a && b)))
+console.log(b);
+var b =2;
+console.log(b);
---
+++ day16.js
@@ -0,0 +1,35 @@
+// console.log("at begining");
+// var number1 =10;
+// var number2 =20;
+
+// console.log("at middle");
+// var number3 =10;
+// var number4 =20;
+// console.log("at end")
+// var number1= 10;
+// var number2=20;
+// if(number2 <= number1){
+//     console.log("number 2is greater than number1");
+// }
+// else{
+//     console.log("for false");
+// }
+// var userAge= 17;
+
+// if(userAge >18){
+//     console.log("user is applicable")
+// }
+// else if (userAge==18){
+//     console.log("User is applicable for learning lic")
+// }
+// else{
+//     console.log("User is not applicable")
+// }
+
+var userNumber = 11 ;
+if((userNumber%2) == 0){
+    console.log("provided number is even");
+} 
+else{
+    console.log("provided number is odd");
+}
