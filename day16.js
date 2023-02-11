f6e34f688d66d679b4c0795f0d7c8a43e64b9658
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-02-11 09:56:56 +0530

    Update day16.js

--- day16.js
+++ day16.js
@@ -1,35 +1,53 @@
-// console.log("at begining");
-// var number1 =10;
-// var number2 =20;
+//if else
+var score=30;
+if(score >35){
+    console.log("You'r Qualified");
+}
+else{
+    console.log(`You'r not qualified with ${score} marks`)
+}
 
-// console.log("at middle");
-// var number3 =10;
-// var number4 =20;
-// console.log("at end")
-// var number1= 10;
-// var number2=20;
-// if(number2 <= number1){
-//     console.log("number 2is greater than number1");
-// }
-// else{
-//     console.log("for false");
-// }
-// var userAge= 17;
+//Driving lic.
+var userAge = 21
+if(userAge >18){
+    console.log("user is applicable");
+}
+else if (userAge==18){
+    console.log("User is applicable for learning lic");
+}
+else{
+    console.log("User is not applicable");
+}
 
-// if(userAge >18){
-//     console.log("user is applicable")
-// }
-// else if (userAge==18){
-//     console.log("User is applicable for learning lic")
-// }
-// else{
-//     console.log("User is not applicable")
-// }
-
-var userNumber = 11 ;
+//Even and odd 
+var userNumber = 12 ;
 if((userNumber%2) == 0){
-    console.log("provided number is even");
+    console.log(`provided number ${userNumber} is even`);
 } 
 else{
-    console.log("provided number is odd");
+    console.log(`provided number ${userNumber} is odd`);
+}
+
+// Switch case
+var ans="pune";
+switch (ans) {
+    case "Dehli":
+        console.log(`${ans} is not capital of maharashtra`);
+        break;
+
+    case ("Mumbai"):
+        console.log(`${ans} answer is correct`);
+            break;
+
+    case "nagpur":
+        console.log(`${ans} is not capital of maharashtra`)
+        break;
+
+    case "Pune":
+        console.log(`${ans} is not capital of maharashtra`)
+        break;
+
+    default:
+        console.log("Select Proper answer")
+        break;
 }
