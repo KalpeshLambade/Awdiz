c75b4d2abb708f5664923caa2aafa61e8afe2e77
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-02-16 10:40:34 +0530

    updated

---
+++ day20.js
@@ -0,0 +1,42 @@
+for(var i= 1; i<=10 ; i++){
+    console.log(i);
+}
+//1st interations
+// step 1 -Starting 1
+// step 2- checking ending; i <=10 ; 1<=10 ; true
+// step 3- code execution ; do anything ; i ko print ;1
+// step 4 -adding 1 into i; then i will be 2
+// result - 1 
+
+//2nd interations
+// step 1 -Starting 2
+// step 2- checking ending; i <=10 ; 2<=10 ; true
+// step 3- code execution ; do anything ; i ko print ;2
+// step 4 -adding 1 into i; then i will be 3
+// result - 2
+
+//3rd interations
+// step 1 -Starting 3
+// step 2- checking ending; i <=10 ; 3<=10 ; true
+// step 3- code execution ; do anything ; i ko print ;3
+// step 4 -adding 1 into i; then i will be 4
+// result - 3
+
+//9th interations
+// step 1 -Starting 9
+// step 2- checking ending; i <=10 ; 9<=10 ; true
+// step 3- code execution ; do anything ; i ko print ;9
+// step 4 -adding 1 into i; then i will be 10
+// result - 9
+
+//10th interations
+// step 1 -Starting 10
+// step 2- checking ending; i <=10 ; 10<=10 ; true
+// step 3- code execution ; do anything ; i ko print ;10
+// step 4 -adding 1 into i; then i will be 11
+// result - 10 
+
+//11th interations
+// step 1 -Starting 11
+// step 2- checking ending; i <=10 ; 11<=10 ; false
+// step 3- code stop
